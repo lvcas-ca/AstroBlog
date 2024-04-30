adfbfc65089410f89c75639a788b9de1bfaf17fc
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2024-04-30 13:26:33 -0300

    confAppwrite

---
+++ Local Sites/astro-word/astro/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: String(process.env.APP_APPWRITE_URL),
+    appwriteProjectId: String(process.env.APP_APPWRITE_PROJECT_ID),
+    appwriteDatabseId: String(process.env.APP_APPWRITE_DATABASE_ID),
+    appwriteCollection: String(process.env.APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(process.env.APP_APPWRITE_BUCKET_ID),
+}
+
+export default conf
