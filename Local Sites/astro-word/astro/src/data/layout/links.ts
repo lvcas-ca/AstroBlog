1b1b71fb7d92685431da26595b137265c31520fb
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2024-05-10 02:18:16 -0300

    Buiding Pages For NAV

---
+++ Local Sites/astro-word/astro/src/data/layout/links.ts
@@ -0,0 +1,20 @@
+export interface Links {
+    title: string;
+    path: string;
+}
+//Array de objetos con los links para llamarl caundo lo necesite
+export const linksData: Links[] = [
+    {
+      title: "Blog",
+      path: "/Blog"
+    },
+    {
+        title: "Poesias",
+        path: "/poesias"
+    },
+    {
+        title: "Historia",
+        path: "/historia"
+    }
+    
+]
